5d9bd2227890bcbc76d97aa06a2382861da9f9a3
rokkerdenis <denis.rokker@gmail.com>
2020-05-01 16:22:01 +0300

    Increase image hash field size

--- migrations/010.add_image_hash.sql
+++ migrations/010.add_image_hash.sql
@@ -1,7 +1,7 @@
 -- mi: 010.add_image_hash.sql
 
 -- TODO: Add NOT NULL constraint
-ALTER TABLE images ADD COLUMN hash VARCHAR(32);
+ALTER TABLE images ADD COLUMN hash VARCHAR(64);
 
 -- TODO: Remove from first migration
 ALTER TABLE images DROP COLUMN path;
