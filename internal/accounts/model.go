ebd58e48b7b8542c5c67e5c6eec8716ed1d17f32
Rokker Ruslan <rokkerruslan@protonmail.com>
2020-05-07 13:43:19 +0300

    Refactor names (resources -> assets)

--- internal/accounts/model.go
+++ internal/accounts/model.go
@@ -15,7 +15,7 @@ var ErrAlreadyExists = errors.New("account already exists")
 func (app *App) createAccount(ctx context.Context, a Account) (Account, error) {
 	baseErr := "createAccount fails: %w"
 
-	err := app.resources.Db.
+	err := app.assets.Db.
 		QueryRow(
 			ctx,
 			"INSERT INTO accounts (name, email, password, is_deleted, created, updated) VALUES ($1, $2, $3, FALSE, NOW(), NOW()) RETURNING id, created, updated",
@@ -40,14 +40,14 @@ func (app *App) createAccount(ctx context.Context, a Account) (Account, error) {
 func (app *App) deleteAccount(ctx context.Context, id int) error {
 	baseErr := "deleteAccount fails: %v"
 
-	if _, err := app.resources.Db.
+	if _, err := app.assets.Db.
 		Exec(ctx, "UPDATE accounts SET is_deleted = TRUE WHERE id = $1", id); err != nil {
-			return fmt.Errorf(baseErr, err)
+		return fmt.Errorf(baseErr, err)
 	}
 
-	if _, err := app.resources.Db.
+	if _, err := app.assets.Db.
 		Exec(ctx, "DELETE FROM tokens WHERE account_id = $1", id); err != nil {
-			return fmt.Errorf(baseErr, err)
+		return fmt.Errorf(baseErr, err)
 	}
 
 	return nil
@@ -58,7 +58,7 @@ var ErrAccountDoesNotExist = errors.New("account does not exist")
 func (app *App) RetrieveByEmail(ctx context.Context, email string) (a Account, err error) {
 	baseErr := "accounts.RetrieveByEmail fails: %v"
 
-	err = app.resources.Db.
+	err = app.assets.Db.
 		QueryRow(
 			ctx,
 			"SELECT id, name, email, password FROM accounts WHERE email = $1 AND is_deleted = FALSE",
@@ -79,13 +79,13 @@ func (app *App) RetrieveByID(ctx context.Context, id int) (Account, error) {
 	baseErr := "accounts.RetrieveByID fails: %v"
 
 	var a Account
-	if err := app.resources.Db.
+	if err := app.assets.Db.
 		QueryRow(
 			ctx,
 			"SELECT id, name, email FROM accounts WHERE id = $1 AND is_deleted = FALSE",
 			id,
 		).Scan(&a.ID, &a.Name, &a.Email); err != nil {
-			return a, fmt.Errorf(baseErr, err)
+		return a, fmt.Errorf(baseErr, err)
 	}
 	return a, nil
 }
