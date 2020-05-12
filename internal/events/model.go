bb29d040911e18a7c9137660a0c12a6fba8a2e1d
Rokker Ruslan <rokkerruslan@protonmail.com>
2020-05-12 15:13:18 +0300

    Refactor events model.go file

--- internal/events/model.go
+++ internal/events/model.go
@@ -9,24 +9,23 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-const createQuery = `
-	INSERT INTO events (name, description, owner_id, created, updated, is_public)
-		VALUES($1, $2, $3, NOW(), NOW(), $4)
-	RETURNING id, created, updated
-`
+func (app *App) eventCreate(ctx context.Context, e Event) (Event, error) {
+	baseErr := "eventCreate fails: %v"
 
-func (app *App) createEvent(ctx context.Context, e Event) (Event, error) {
-	baseErr := "createEvent fails: %v"
+	q := `
+		INSERT INTO events (name, description, owner_id, created, updated, is_public)
+			VALUES($1, $2, $3, NOW(), NOW(), $4)
+		RETURNING id, created, updated
+	`
 
-	err := app.assets.Db.QueryRow(
+	if err := app.assets.Db.QueryRow(
 		ctx,
-		createQuery,
+		q,
 		e.Name,
 		e.Description,
 		e.OwnerID,
 		e.IsPublic,
-	).Scan(&e.ID, &e.Created, &e.Updated)
-	if err != nil {
+	).Scan(&e.ID, &e.Created, &e.Updated); err != nil {
 		return e, fmt.Errorf(baseErr, err)
 	}
 
@@ -47,56 +46,50 @@ func (app *App) createEvent(ctx context.Context, e Event) (Event, error) {
 	return e, nil
 }
 
-const updateQuery = `
-	UPDATE events SET name = $1, updated = NOW(), is_hidden = $3, description = $4 WHERE id = $2
-`
+func (app *App) eventUpdate(ctx context.Context, e Event) error {
+	baseErr := "eventUpdate fails: %v"
 
-func (app *App) updateEvent(ctx context.Context, e Event) error {
-	baseErr := "event.Update fails: %v"
-
-	_, err := app.assets.Db.Exec(ctx, updateQuery, e.Name, e.ID, e.IsHidden, e.Description)
-	if err != nil {
+	if _, err := app.assets.Db.Exec(
+		ctx,
+		"UPDATE events SET name = $1, updated = NOW(), is_hidden = $3, description = $4 WHERE id = $2",
+		e.Name,
+		e.ID,
+		e.IsHidden,
+		e.Description,
+	); err != nil {
 		return fmt.Errorf(baseErr, err)
 	}
 	return nil
 }
 
-const retrieveQuery = `
-	SELECT id, name, owner_id, created, updated FROM events WHERE id = $1
-`
+func (app *App) eventRetrieve(ctx context.Context, f api.RetrieveFilter) (e Event, err error) {
+	baseErr := "eventRetrieve fails: %v"
 
-func (app *App) retrieveEvent(ctx context.Context, f api.RetrieveFilter) (e Event, err error) {
-	defErr := "modelRetrieve fails: %v"
-	err = app.assets.Db.QueryRow(ctx, retrieveQuery, f.ID).Scan(e.ID, e.Name, e.OwnerID, e.Created, e.Updated)
-	if err != nil {
-		return e, fmt.Errorf(defErr, err)
+	if err = app.assets.Db.QueryRow(
+		ctx,
+		"SELECT id, name, owner_id, created, updated FROM events WHERE id = $1",
+		f.ID,
+	).Scan(e.ID, e.Name, e.OwnerID, e.Created, e.Updated); err != nil {
+		return e, fmt.Errorf(baseErr, err)
 	}
 	return e, nil
 }
 
-const deleteQuery = `
-	DELETE FROM events WHERE id = $1
-`
+func (app *App) eventDelete(ctx context.Context, eventID int) error {
+	baseErr := "eventDelete fails: %v"
 
-func (app *App) deleteEvent(ctx context.Context, f api.RetrieveFilter) error {
-	defErr := "modelDelete fails: %v"
-	_, err := app.assets.Db.Exec(ctx, deleteQuery, f.ID)
-	if err != nil {
-		return fmt.Errorf(defErr, err)
+	if _, err := app.assets.Db.Exec(
+		ctx,
+		"UPDATE events SET is_deleted = TRUE WHERE id = $1",
+		eventID,
+	); err != nil {
+		return fmt.Errorf(baseErr, err)
 	}
 	return nil
 }
 
-const selectQuery = `
-	SELECT id, name, description, owner_id, created, updated, is_public, is_hidden FROM events LIMIT 10
-`
-
-const selectTimelinesQuery = `
-	SELECT id, start, "end", place, event_id FROM timelines WHERE event_id = ANY($1)
-`
-
 func (app *App) eventList(ctx context.Context, f api.AccountAndEventFilter) ([]Event, error) {
-	baseErr := "event.listHandler fails: %v"
+	baseErr := "eventList fails: %v"
 
 	var err error
 	var rows pgx.Rows
@@ -131,7 +124,11 @@ func (app *App) constructEventList(ctx context.Context, rows pgx.Rows) ([]Event,
 		events = append(events, e)
 	}
 
-	timelineRows, err := app.assets.Db.Query(ctx, selectTimelinesQuery, eventIDs)
+	timelineRows, err := app.assets.Db.Query(
+		ctx,
+		`SELECT id, start, "end", place, event_id FROM timelines WHERE event_id = ANY($1)`,
+		eventIDs,
+	)
 	if err != nil {
 		return nil, err
 	}
