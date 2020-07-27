fad61a8b8b464448d7e5a5269410788024bfc6a2
rokkerdenis <denis.rokker@gmail.com>
2020-07-27 12:42:37 +0300

    test optimization

--- tests/phd_test.py
+++ tests/phd_test.py
@@ -2,20 +2,11 @@ import pytest
 import requests
 import psycopg2
 
+HOST = "http://localhost:3000"
 
-def find_test(variables):
-    """
-    :param dict variables:
-    """
 
-    functions = []
-
-    for name in variables:
-        function = variables[name]
-        if name.startswith("test_") and callable(function):
-            functions.append(function)
-
-    return functions
+def teardown_function():
+    delete_all_db()
 
 
 def delete_all_db():
@@ -37,77 +28,59 @@ def delete_all_db():
     connect.close()
 
 
-def test_sign_up_200ok():
-    delete_all_db()
+delete_all_db()
 
+account_info = {
+    "Email": "test1@gmail.com",
+    "Password": "1",
+    "Name": "test1"
+}
+
+
+def test_sign_up_200():
     r = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
     assert r.status_code == 200, r.text
 
-    delete_all_db()
-
 
 def test_sign_up_400_password_length_check_fails():
-    delete_all_db()
-
     r = requests.post(
-        "http://localhost:3000/accounts/sign-up",
+        f"{HOST}/accounts/sign-up",
         json={
             "Email": "test1@gmail.com",
             "Password": "",
             "name": "test1"
         }
     )
+
     assert r.status_code == 400, r.text
+    assert "Password" in r.text
 
-    delete_all_db()
 
-
-def test_sign_up_400br_account_already_exists():
-    delete_all_db()
-
+def test_sign_up_400_account_already_exists():
     r = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
     assert r.status_code == 200, r.text
 
     r = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
     assert r.status_code == 400, r.text
+    assert "already" in r.text
 
-    delete_all_db()
 
-
-def test_sign_out_204ok():
-    delete_all_db()
-
+def test_sign_out_204():
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
     assert sign_up_response.status_code == 200, sign_up_response.text
@@ -119,165 +92,111 @@ def test_sign_out_204ok():
         headers=x_auth_token
     )
 
-    assert sign_out_response.status_code == 204
-
-    delete_all_db()
+    assert sign_out_response.status_code == 204, sign_out_response.text
 
 
-def test_sign_in_200ok():
-    delete_all_db()
-
+def test_sign_in_200():
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
+        f"{HOST}/accounts/sign-up",
+        json=account_info
+    )
+
+    assert sign_up_response.status_code == 200, sign_up_response.text
+
+    sign_in_response = requests.post(
+        f"{HOST}/accounts/sign-in",
+        json=account_info
+    )
+
+    assert sign_in_response.status_code == 200, sign_in_response.text
+
+
+def test_sign_in_400_wrong_password():
+    sign_up_response = requests.post(
+        f"{HOST}/accounts/sign-up",
+        json=account_info
+    )
+
+    assert sign_up_response.status_code == 200, sign_up_response.text
+
+    sign_in_response = requests.post(
+        f"{HOST}/accounts/sign-in",
         json={
             "Email": "test1@gmail.com",
-            "Password": "1",
+            "Password": "2",
             "name": "test1"
         }
     )
 
-    assert sign_up_response.status_code == 200
-
-    sign_in_response = requests.post(
-        "http://localhost:3000/accounts/sign-in",
-        json={"Email": "test1@gmail.com",
-              "Password": "1"
-              }
-    )
-
-    assert sign_in_response.status_code == 200
-
-    delete_all_db()
+    assert sign_in_response.status_code == 400, sign_in_response.text
+    assert "password" in sign_in_response.text
 
 
-def test_sign_in_400br_wrong_password():
-    delete_all_db()
-
-    sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
-    )
-
-    assert sign_up_response.status_code == 200
-
-    sign_in_response = requests.post(
-        "http://localhost:3000/accounts/sign-in",
-        json={"Email": "test1@gmail.com",
-              "Password": "2"
-              }
-    )
-
-    assert sign_in_response.status_code == 400
-
-    delete_all_db()
-
-
-def test_sign_in_400br_account_does_not_exist():
+def test_sign_in_400_account_does_not_exist():
     """
 
     Запрещено осуществлять вход в систему, не зарегистрировавшись.
     """
-    delete_all_db()
 
+    r = requests.post(
+        f"{HOST}/accounts/sign-in",
+        json=account_info
+    )
+    assert r.status_code == 400, r.text
+    assert "exist" in r.text
+
+
+def test_account_info_200():
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
-    assert sign_up_response.status_code == 200
-
-    sign_in_response = requests.post(
-        "http://localhost:3000/accounts/sign-in",
-        json={"Email": "test2@gmail.com",
-              "Password": "2"
-              }
-    )
-    assert sign_in_response.status_code == 400
-
-    delete_all_db()
-
-
-def test_info_200ok():
-    delete_all_db()
-
-    sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
-    )
-
-    assert sign_up_response.status_code == 200
+    assert sign_up_response.status_code == 200, sign_up_response.text
 
     account_id = sign_up_response.json()["Account"]["ID"]
     x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
 
     info_response = requests.get(
-        f"http://localhost:3000/accounts/{account_id}",
+        f"{HOST}/accounts/{account_id}",
         headers=x_auth_token
     )
 
-    assert info_response.status_code == 200
-
-    delete_all_db()
+    assert info_response.status_code == 200, info_response.text
 
 
-def test_delete_account_200ok():
-    delete_all_db()
-
+def test_delete_account_200():
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
-    assert sign_up_response.status_code == 200
+    assert sign_up_response.status_code == 200, sign_up_response.text
 
     account_id = sign_up_response.json()["Account"]["ID"]
     x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
 
     delete_account_response = requests.delete(
-        f"http://localhost:3000/accounts/{account_id}",
+        f"{HOST}/accounts/{account_id}",
         headers=x_auth_token
     )
 
-    assert delete_account_response.status_code == 204
-
-    delete_all_db()
+    assert delete_account_response.status_code == 204, delete_account_response.text
 
 
-def create_event_200ok():
-    delete_all_db()
-
+def create_event_200():
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
-    assert sign_up_response.status_code == 200
+    assert sign_up_response.status_code == 200, sign_up_response.text
 
     account_id = sign_up_response.json()["Account"]["ID"]
     x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
 
     create_event_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
+        f"{HOST}/accounts/sign-up",
         headers=x_auth_token,
         json={
             "Name": "1",
@@ -293,33 +212,28 @@ def create_event_200ok():
         }
     )
 
-    assert create_event_response.status_code == 200
+    assert create_event_response.status_code == 200, create_event_response.text
 
 
 @pytest.mark.xfail(reason="issue #37")
-def test_create_offer_400br():
+def test_create_offer_400():
     """
 
     По бизнес-логике запрещено создавать два офера на один ивент.
     """
-    delete_all_db()
 
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
-        json={
-            "Email": "test1@gmail.com",
-            "Password": "1",
-            "name": "test1"
-        }
+        f"{HOST}/accounts/sign-up",
+        json=account_info
     )
 
-    assert sign_up_response.status_code == 200
+    assert sign_up_response.status_code == 200, sign_up_response.text
 
     account_id_1 = sign_up_response.json()["Account"]["ID"]
     account_1_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
 
     sign_up_response = requests.post(
-        "http://localhost:3000/accounts/sign-up",
+        f"{HOST}/accounts/sign-up",
         json={
             "Email": "test2@gmail.com",
             "Password": "2",
@@ -327,13 +241,13 @@ def test_create_offer_400br():
         }
     )
 
-    assert sign_up_response.status_code == 200
+    assert sign_up_response.status_code == 200, sign_up_response.text
 
     account_id_2 = sign_up_response.json()["Account"]["ID"]
     account_2_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
 
     create_events_response = requests.post(
-        "http://localhost:3000/events",
+        f"{HOST}/events",
         headers=account_1_token,
         json={
             "Name": "1",
@@ -349,12 +263,12 @@ def test_create_offer_400br():
         }
     )
 
-    assert create_events_response.status_code == 200
+    assert create_events_response.status_code == 200, create_events_response.text
 
     event_id = create_events_response.json()["ID"]
 
     create_offer_response_1 = requests.post(
-        "http://localhost:3000/offers",
+        f"{HOST}/offers",
         headers=account_2_token,
         json={
             "AccountID": account_id_2,
@@ -362,23 +276,15 @@ def test_create_offer_400br():
         }
     )
 
-    assert create_offer_response_1.status_code == 200
+    assert create_offer_response_1.status_code == 200, create_offer_response_1.text
 
     create_offer_response_2 = requests.post(
-        "http://localhost:3000/offers",
+        f"{HOST}/offers",
         headers=account_2_token,
         json={
             "AccountID": account_id_2,
             "EventID": event_id
         }
     )
-    assert create_offer_response_2.status_code == 400
-
-    delete_all_db()
-
-
-if __name__ == '__main__':
-
-    find_test_list = find_test(locals().copy())
-    for test in find_test_list:
-        test()
+    assert create_offer_response_2.status_code == 400, create_offer_response_2.text
+    assert "fails" in create_offer_response_2.text
