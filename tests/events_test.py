7b471e1304dcd15a6669665e1efc71284fd6f1ee
rokkerdenis <denis.rokker@gmail.com>
2020-08-03 15:43:21 +0300

    add new events and update events tests

--- tests/events_test.py
+++ tests/events_test.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from datetime import datetime
 from tests import delete_all_db, create_valid_account_info, create_valid_event_info, HOST
 
 
@@ -137,43 +138,6 @@ def test_create_event_400_didnt_authorized():
     assert "authorized" in create_event_response.text
 
 
-def test_update_events_200():
-    """
-    Тест проверяет функцию обновления данных ивента с валидными данными.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    event = create_valid_event_info(account_id)
-    del event["OwnerID"]
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    assert update_event_response.status_code == 200, update_event_response.text
-
-
 def test_event_info_200():
     """
     Тест проверяет функцию получения информации об ивенте.
@@ -270,9 +234,48 @@ def test_check_timlines_not_nil():
     assert timlelines
 
 
-def test_update_events_400_name_is_empty():
+def test_create_events_400_start_after_end():
     """
-    Тест проверяет возникновение ошибки при обновлении ивента без ввода имени ивента.
+
+    """
+    sign_up_response = requests.post(
+        f"{HOST}/accounts/sign-up",
+        json=create_valid_account_info()
+    )
+
+    assert sign_up_response.status_code == 200, sign_up_response.text
+
+    account_id = sign_up_response.json()["Account"]["ID"]
+    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
+
+    create_event_response = requests.post(
+        f"{HOST}/events",
+        headers=x_auth_token,
+        json={
+            "Name": "1",
+            "Description": "1",
+            "OwnerID": account_id,
+            "IsPublic": False,
+            "isHidden": False,
+            "Timelines": [
+                {
+                    "Start": "2006-01-02T17:05:05Z",
+                    "End": "2005-01-02T18:06:05Z",
+                    "Place": "Saint Petersburg"
+                }
+            ]
+        }
+    )
+
+    assert create_event_response.status_code == 400, create_event_response.text
+    assert "Start" in create_event_response.text
+
+
+@pytest.mark.xfail(reason="issue #57")
+def test_create_events_400_timeline_is_not_in_the_past():
+    """
+    Тест проверяет возниконовение ошибки при создании ивента с таймлайном который находится в прошедшем
+    промежутке времени.
     """
     sign_up_response = requests.post(
         f"{HOST}/accounts/sign-up",
@@ -290,353 +293,51 @@ def test_update_events_400_name_is_empty():
         headers=x_auth_token,
         json=info
     )
+    event = create_event_response.json()
 
-    assert create_event_response.status_code == 200, create_event_response.text
+    created = datetime.strptime(event["Created"], "%Y-%m-%dT%H:%M:%S.%fZ")
+    start = datetime.strptime(event["Timelines"][0]["Start"], "%Y-%m-%dT%H:%M:%SZ")
+    end = datetime.strptime(event["Timelines"][0]["End"], "%Y-%m-%dT%H:%M:%SZ")
 
-    event_id = create_event_response.json()["ID"]
+    assert create_event_response.status_code == 400, create_event_response.text
+    assert created > start and end
 
+
+@pytest.mark.xfail()
+def test_create_events_400_intersection_of_timelines():
+    """
+    Тест проверяет возниконовение ошибки при пересечении временнх промежутков.
+    """
+    sign_up_response = requests.post(
+        f"{HOST}/accounts/sign-up",
+        json=create_valid_account_info()
+    )
+
+    assert sign_up_response.status_code == 200, sign_up_response.text
+
+    account_id = sign_up_response.json()["Account"]["ID"]
+    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
     event = create_valid_event_info(account_id)
-    event["Name"] = ""
-    del event["OwnerID"]
+    event["Timelines"].append(
+        {
+            "Start": "2018-01-02T17:05:05Z",
+            "End": "2018-01-02T18:06:05Z",
+            "Place": "Saint Petersburg"
+        }
+    )
 
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
+    create_event_response = requests.post(
+        f"{HOST}/events",
         headers=x_auth_token,
         json=event
     )
 
-    assert update_event_response.status_code == 400, update_event_response.text
-    assert "Title" in update_event_response.text
-
-
-def test_update_events_400_description_is_empty():
-    """
-    Тест проверяет возникновение ошибки при обновлении ивента без ввода описания ивента.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    event = create_valid_event_info(account_id)
-    event["Description"] = ""
-    del event["OwnerID"]
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    assert update_event_response.status_code == 400, update_event_response.text
-    assert "Description" in update_event_response.text
-
-
-@pytest.mark.xfail(reason="issue #52")
-def test_update_events_400_timelines_is_empty():
-    """
-    Тест проверяет возникновение ошибки при обновлении ивента без ввода таймлайна ивента. issue #52
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    event = create_valid_event_info(account_id)
-    event["Timeline"] = []
-    del event["OwnerID"]
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    assert update_event_response.status_code == 400, update_event_response.text
-    assert "Timeline" in update_event_response.text
-
-
-@pytest.mark.xfail
-def test_update_events_400_not_authorized():
-    """
-    Запрещено обновлять ивент без авторизации.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        json=info
-    )
-
-    assert update_event_response.status_code == 400, update_event_response.text
-
-
-@pytest.mark.xfail
-def test_update_events_400_id_doesnt_match():
-    """
-    Запрещено обновлять ивент без авторизации.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-    info['OwnerID'] = account_id + 1
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert update_event_response.status_code == 400, update_event_response.text
-    assert "authorized" in update_event_response.text
-
-
-@pytest.mark.xfail
-def test_update_events_owner_id_not_nil():
-    """
-    ID аккаунта не может ровняться нулю.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    event = create_valid_event_info(account_id)
-    del event["OwnerID"]
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    assert update_event_response.status_code == 200, update_event_response.text
-    assert update_event_response.json()["OwnerID"] != 0
-
-
-@pytest.mark.xfail
-def test_update_events_info():
-    """
-    Тест проверяет обновление информации ивента.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    event = create_valid_event_info(account_id)
-    event["Name"] += "test"
-    event["Description"] += "test"
-    event["IsPublic"] = True
-    event["IsHidden"] = True
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    assert update_event_response.status_code == 200, update_event_response.text
-
-    event_info_response = requests.get(f"{HOST}/events/{event_id}")
-    event_info = event_info_response.json()
-
-    assert event_info_response.status_code == 200, event_info_response.text
-    assert event_info["Name"] == event["Name"]
-    assert event_info["Description"] == event["Description"]
-    assert event_info["IsPublic"] == event["IsPublic"]
-    assert event_info["IsHidden"] == event["IsHidden"]
-
-
-@pytest.mark.xfail
-def test_update_events_timelines():
-    """
-    Тест проверяет обновление данных в таймлайне ивента.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    event = create_valid_event_info(account_id)
-    event["Timelines"][0]["Start"] = "2021-08-02T13:43:09.535504Z"
-    event["Timelines"][0]["End"] = "2021-08-03T13:43:09.535504Z"
-    event["Timelines"][0]["Place"] = "Moscow"
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    assert update_event_response.status_code == 200, update_event_response.text
-
-    event_info_response = requests.get(f"{HOST}/events/{event_id}")
-    event_info = event_info_response.json()
-
-    assert event_info_response.status_code == 200, event_info_response.text
-    assert event_info["Timelines"] == event["Timelines"]
-
-
-@pytest.mark.xfail
-def test_update_events_added_two_timelines():
-    """
-    Тест проверяет добавление дополнительного таймлана в ивент.
-    """
-    sign_up_response = requests.post(
-        f"{HOST}/accounts/sign-up",
-        json=create_valid_account_info()
-    )
-
-    assert sign_up_response.status_code == 200, sign_up_response.text
-
-    account_id = sign_up_response.json()["Account"]["ID"]
-    x_auth_token = {"X-Auth-Token": sign_up_response.json()["Token"]}
-    info = create_valid_event_info(account_id)
-
-    create_event_response = requests.post(
-        f"{HOST}/events",
-        headers=x_auth_token,
-        json=info
-    )
-
-    assert create_event_response.status_code == 200, create_event_response.text
-
-    event_id = create_event_response.json()["ID"]
-
-    timelines_2 = {
-                "Start": "2021-01-02T17:05:05Z",
-                "End": "2021-01-02T18:06:05Z",
-                "Place": "Saint Petersburg"
-            }
-
-    event = create_valid_event_info(account_id)
-    event["Timelines"].append(timelines_2)
-    del event["OwnerID"]
-
-    update_event_response = requests.put(
-        f"{HOST}/events/{event_id}",
-        headers=x_auth_token,
-        json=event
-    )
-
-    event_info = update_event_response.json()
-
-    assert update_event_response.status_code == 200, update_event_response.text
-    assert len(event_info["Timelines"]) == 2
+    create_event = create_event_response.json()
+
+    start_1 = datetime.strptime(create_event["Timelines"][0]["Start"], "%Y-%m-%dT%H:%M:%SZ")
+    end_1 = datetime.strptime(create_event["Timelines"][0]["End"], "%Y-%m-%dT%H:%M:%SZ")
+    start_2 = datetime.strptime(create_event["Timelines"][1]["Start"], "%Y-%m-%dT%H:%M:%SZ")
+    end_2 = datetime.strptime(create_event["Timelines"][1]["End"], "%Y-%m-%dT%H:%M:%SZ")
+
+    assert create_event_response.status_code == 400, create_event_response.text
+    assert start_1 and end_1 > end_2 or start_1 and end_1 < start_2
